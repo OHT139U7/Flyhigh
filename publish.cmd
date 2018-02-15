048366e5a567d43482d05a57fb51217b4cac7a2f
OHT139U7 <36498587+OHT139U7@users.noreply.github.com>
2018-02-15 14:16:11 -0800

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=flyhighnlp-Web-Deploy.pubxml -p:Password=TLMmxJ3J1SbBblDwKGNxbDtB8xgfa5kRghiftseufnTr38FjAxRniek1Yyad
+
